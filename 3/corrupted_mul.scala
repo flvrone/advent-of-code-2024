7508d3977b559299f0657d9de99bdcd42bac0010
Nazar Matus <funkyloverone@gmail.com>
2024-12-03 22:52:57 +0200

    Add day 3 solutions

---
+++ 3/corrupted_mul.scala
@@ -0,0 +1,25 @@
+//> using scala 3.6.1
+//> using toolkit 0.6.0
+
+import scala.util.matching.Regex
+
+@main
+def main1(inputFileName: String): Unit =
+  val path: os.Path = os.pwd / os.SubPath(inputFileName)
+  val lines: Seq[String] = os.read.lines(path)
+
+  println(lines.map(parseAndSum).reduceLeft(_ + _))
+
+val MulPattern: Regex = raw"mul\((\d{1,3}),(\d{1,3})\)".r
+
+def parseAndSum(command: String): Int =
+  sumOfMults(parsePairs(command))
+
+def parsePairs(command: String): List[(Int, Int)] =
+  MulPattern.findAllMatchIn(command).toList.map(
+    (patternMatch) =>
+      (patternMatch.group(1).toInt, patternMatch.group(2).toInt)
+    )
+
+def sumOfMults(pairs: List[(Int, Int)]): Int =
+  pairs.map((a, b) => a * b).reduceLeft(_ + _)
---
+++ 3/dos_and_donts.scala
@@ -0,0 +1,35 @@
+//> using scala 3.6.1
+//> using toolkit 0.6.0
+
+package advent24.dosanddonts
+
+import scala.util.matching.Regex
+
+@main
+def main2(inputFileName: String): Unit =
+  val path: os.Path = os.pwd / os.SubPath(inputFileName)
+  val lines: Seq[String] = os.read.lines(path)
+
+  // Easy solution, but that combined string is probably too long?
+  println(parseAndSum(lines.mkString))
+
+val MulPattern: Regex = raw"mul\((\d{1,3}),(\d{1,3})\)".r
+val DoPattern: String = raw"do\(\)"
+val DontPattern: String = raw"don't\(\)"
+
+def parseAndSum(command: String): Int =
+  sumOfMults(parsePairs(filterCommand(command)))
+
+def parsePairs(command: String): List[(Int, Int)] =
+  MulPattern.findAllMatchIn(command).toList.map(
+    (patternMatch) =>
+      (patternMatch.group(1).toInt, patternMatch.group(2).toInt)
+    )
+
+def sumOfMults(pairs: List[(Int, Int)]): Int =
+  pairs.map((a, b) => a * b).reduceLeft(_ + _)
+
+def filterCommand(command: String): String =
+  command .split(DoPattern)
+          .map(_.split(DontPattern, 2).apply(0))
+          .mkString
