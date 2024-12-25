85e7381e52084990cf71885346ffd9e250a7a187
Nazar Matus <funkyloverone@gmail.com>
2024-12-25 23:31:57 +0200

    fixup! Add day 11 pt. 1 solution

--- 11/plutonian_pebbles.scala
+++ 11/plutonian_pebbles.scala
@@ -10,25 +10,25 @@ def main1(inputFileName: String): Unit =
   val path: os.Path = os.pwd / os.SubPath(inputFileName)
   val lines: Seq[String] = os.read.lines(path)
 
-  val nums = lines.apply(0).split("\\s+").map(_.toLong).toList
+  val nums = lines.apply(0).split("\\s+").map(_.toLong).toVector
 
   println(blink(nums, 25).length)
 
 @tailrec
-def blink(nums: List[Long], iterations: Int = 1): List[Long] =
+def blink(nums: Vector[Long], iterations: Int = 1): Vector[Long] =
   if iterations <= 0 then nums
   else
     val newNums = nums.flatMap(num =>
-      if num == 0L then List(1L)
+      if num == 0L then Vector(1L)
       else
         val str = num.toString()
         if isEven(str.length()) then
           middleSplitStr(str).toList.map(_.toLong)
-        else List(num * 2024)
+        else Vector(num * 2024L)
     )
     blink(newNums, iterations - 1)
 
-def isEven(num: Int): Boolean = num % 2 ==0
+def isEven(num: Int): Boolean = num % 2 == 0
 
 def middleSplitStr(str: String): (String, String) =
   str.splitAt(str.length() / 2)
